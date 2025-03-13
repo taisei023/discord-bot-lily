a65eed057de8cdd8bb0ad0539216b314aa654c71
taisei023 <tm.lovin8@gmail.com>
2025-03-13 17:40:43 +0900

    Initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+VOICE_CHANNEL_ID = 847514073964740679
+
+ADMIN_ROLE_ID = 946432769242845224
+
+MEIBO_CHANNEL_ID = 847686946268512287
